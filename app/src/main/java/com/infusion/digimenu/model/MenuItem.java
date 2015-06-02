24c569708c72bc94e71ab8bc8d83cefeabcde4d6
Anthony Li <anthonycsli@hotmail.com>
2015-06-01 22:09:49 -0400

    Update MenuItem schema to include a unique identifier.

--- app/src/main/java/com/infusion/digimenu/model/MenuItem.java
+++ app/src/main/java/com/infusion/digimenu/model/MenuItem.java
@@ -6,14 +6,16 @@ import java.math.BigDecimal;
 /**
  * Created by ali on 2015-05-30.
  */
-public class MenuItem implements Serializable{
+public class MenuItem implements Serializable {
+    public int id;
     public String name;
     public String description;
     public boolean isSpicy;
     public String imageUri;
     public BigDecimal price;
 
-    public MenuItem(String name, String description, boolean isSpicy, String imageUri, BigDecimal price) {
+    public MenuItem(int id, String name, String description, boolean isSpicy, String imageUri, BigDecimal price) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.isSpicy = isSpicy;
